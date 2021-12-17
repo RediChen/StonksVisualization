ee12953615aba08db13bbb831717d8b42fc1b288
RediChen <a0929052958@gmail.com>
2021-12-17 12:44:34 +0800

    建立頁面基本 layout 與樣式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 
 const App = () => {
   return (
-    <div className="bg">
-      <div className="panel">
-        <h1>TEST</h1>
+    <div className="panel">
+      <h1>TEST</h1>
+      <div className="wrap">
+        <div className="monitor">
+          <div></div>
+        </div>
+        <div className="monitor">
+          <div></div>
+        </div>
+        <div className="monitor">
+          <div></div>
+        </div>
+        <div className="monitor">
+          <div></div>
+        </div>
       </div>
     </div>
   );
