2669e711b445191e6713fc388198a3143105f69e
RediChen <a0929052958@gmail.com>
2022-01-14 23:19:35 +0800

    將 react vis 的 css 改由 tsx 匯入

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/style.css';
+import "../node_modules/react-vis/dist/style"
 
 ReactDOM.render(
   <React.StrictMode>
