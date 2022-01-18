fcf94912f6c30031e0c759eb2e3a03da2bc6eaae
RediChen <a0929052958@gmail.com>
2022-01-18 13:55:13 +0800

    引入互動機制
    能隨滑鼠位置即時讀取位置
    但是元件沒有正常顯示

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
-import {LineSeries, XYPlot} from "react-vis";
+import { useState } from "react";
+import {
+  LineSeries, XYPlot, Crosshair
+} from "react-vis";
 import dataTemp from './data/data-temp'
 
-const dataHandled = dataTemp.map(data => ({x: data.x, y: data.y}));
+const dataHandled = dataTemp.map(data => ({ x: data.x, y: data.y }));
 
 // todo 實作互動機制 onWhatever
 
 const App = () => {
+  const [focus, setFocus] = useState<number[]>([]);
   return (
     <div className="panel">
-      <h1>TEST</h1>
       <div className="wrap">
         <div className="monitor">
-            <XYPlot width={300} height={200}>
-              <LineSeries data={dataHandled} />
-            </XYPlot>
+          <XYPlot width={900} height={400} onMouseLeave={() => setFocus([])}>
+            <LineSeries data={dataHandled}
+              onNearestX={(value, { innerX }) => setFocus([innerX])} />
+            <Crosshair value={focus}>
+              {/* <div style={{ backgroundColor: "#fff", width: '50px', borderRadius: '50%' }}>ahhh</div> */}
+              {/* <div style={{ background: 'black' }}>
+                <h3>Values of crosshair:</h3>
+                <p>{focus} NTD</p>
+              </div> */}
+            </Crosshair>
+          </XYPlot>
         </div>
       </div>
+      <h1>TEST: {focus}</h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './style/style.css';
-import "../node_modules/react-vis/dist/style"
+import "../node_modules/react-vis/dist/style.css"
 
 ReactDOM.render(
   <React.StrictMode>
